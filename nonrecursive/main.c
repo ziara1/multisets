9ac119cd2b6f9147b70cb25a00466b1801e3827c
ziara1 <143950916+ziara1@users.noreply.github.com>
2025-01-05 12:23:30 +0100

    0 malloc najszybsze na razie

--- nonrecursive/main.c
+++ nonrecursive/main.c
@@ -1,31 +1,24 @@
 #include <stddef.h>
-#include <stdlib.h>  // Dla funkcji malloc, free
 #include "common/io.h"
 #include "common/sumset.h"
 
 typedef struct {
     Sumset *a, *b;
     size_t indexA, indexB;
-    bool firstA, firstB;
+    Sumset sumsetA, sumsetB;
 } StackFrame;
 
 void solve_iterative(InputData* input_data, Solution* best_solution) {
-    // Rozmiar stosu: maksymalna liczba stanów DFS
-    size_t stack_capacity = input_data->d * input_data->d;
     size_t stack_size = 0;
-    StackFrame* stack = malloc(stack_capacity * sizeof(StackFrame));
-    if (!stack) exit(1); // Błąd alokacji
+    StackFrame stack[512];
     stack[stack_size++] = (StackFrame){
-        .a = malloc(sizeof(Sumset)),
-        .b = malloc(sizeof(Sumset)),
+        .sumsetA = input_data->a_start,
+        .sumsetB = input_data->b_start,
         .indexA = input_data->a_start.last,
-        .indexB = input_data->b_start.last,
-        .firstA = true,
-        .firstB = true
+        .indexB = input_data->b_start.last
     };
-    if (!stack[0].a || !stack[0].b) exit(1); // Błąd alokacji
-    *(stack[0].a) = input_data->a_start;
-    *(stack[0].b) = input_data->b_start;
+    stack[0].a = &stack[0].sumsetA;
+    stack[0].b = &stack[0].sumsetB;
 
     while (stack_size > 0) {
         const size_t s = stack_size - 1;
@@ -33,52 +26,35 @@ void solve_iterative(InputData* input_data, Solution* best_solution) {
             Sumset* temp = stack[s].a;
             stack[s].a = stack[s].b;
             stack[s].b = temp;
-
             size_t temp_index = stack[s].indexA;
             stack[s].indexA = stack[s].indexB;
             stack[s].indexB = temp_index;
-
-            bool tempFirstA = stack[s].firstA;
-            stack[s].firstA = stack[s].firstB;
-            stack[s].firstB = tempFirstA;
         }
 
-
         if (stack[s].indexA > input_data->d) {
-            if (stack[s].firstA)
-                free(stack[s].a);
-            if (stack[s].firstB)
-                free(stack[s].b);
             --stack_size;
             continue;
         }
         if (is_sumset_intersection_trivial(stack[s].a, stack[s].b)) {
             const size_t i = stack[s].indexA;
             if (!does_sumset_contain(stack[s].b, i)) {
-                Sumset* a_with_i = malloc(sizeof(Sumset));
-                if (!a_with_i) exit(1); // Błąd alokacji
-                sumset_add(a_with_i, stack[s].a, i);
                 stack[stack_size++] = (StackFrame){
-                    .a = a_with_i,
                     .b = stack[s].b,
                     .indexA = i,
-                    .indexB = stack[s].indexB,
-                    .firstA = true,
-                    .firstB = false
+                    .indexB = stack[s].indexB
                 };
+                sumset_add(&stack[stack_size - 1].sumsetA, stack[s].a, i);
+                stack[stack_size - 1].a = &stack[stack_size - 1].sumsetA;
             }
         } else if ((stack[s].a->sum == stack[s].b->sum) &&
                    (get_sumset_intersection_size(stack[s].a, stack[s].b) == 2)) {
             if (stack[s].b->sum > best_solution->sum) {
                 solution_build(best_solution, input_data, stack[s].a, stack[s].b);
             }
-                   }
+        }
         stack[s].indexA++;
     }
-    free(stack);
 }
-
-
 
 int main()
 {
@@ -93,5 +69,6 @@ int main()
 
     return 0;
 }
+
 
 
