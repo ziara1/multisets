11677daf18966d22b6761201a574ae9b4a5deee3
ziara1 <143950916+ziara1@users.noreply.github.com>
2025-01-05 13:12:18 +0100

    0 MALLOC 2 STOSY ZOPTYMALIZOWANE
    na stacku trzyma 2 boole pamietajace wynik ifow zeby nie liczyc wiele razy

--- nonrecursive/main.c
+++ nonrecursive/main.c
@@ -6,6 +6,7 @@ typedef struct {
     Sumset *a, *b;
     size_t indexA, indexB;
     size_t onStack;
+    bool trivial, elif;
 } StackFrame;
 
 void solve_iterative(InputData* input_data, Solution* best_solution) {
@@ -21,6 +22,8 @@ void solve_iterative(InputData* input_data, Solution* best_solution) {
         .indexA = input_data->a_start.last,
         .indexB = input_data->b_start.last,
         .onStack = 2,
+        .trivial = false,
+        .elif = false
     };
 
     while (stack_size > 0) {
@@ -39,7 +42,8 @@ void solve_iterative(InputData* input_data, Solution* best_solution) {
             sumsetStack_size -= stack[s].onStack;
             continue;
         }
-        if (is_sumset_intersection_trivial(stack[s].a, stack[s].b)) {
+        if (stack[s].trivial || is_sumset_intersection_trivial(stack[s].a, stack[s].b)) {
+            stack[s].trivial = true;
             const size_t i = stack[s].indexA;
             if (!does_sumset_contain(stack[s].b, i)) {
                 sumset_add(&sumsetStack[sumsetStack_size++], stack[s].a, i);
@@ -49,10 +53,12 @@ void solve_iterative(InputData* input_data, Solution* best_solution) {
                     .indexA = i,
                     .indexB = stack[s].indexB,
                     .onStack = 1,
+                    .trivial = false,
+                    .elif = false
                 };
             }
-        } else if ((stack[s].a->sum == stack[s].b->sum) &&
-                   (get_sumset_intersection_size(stack[s].a, stack[s].b) == 2)) {
+        } else if (stack[s].elif || ((stack[s].a->sum == stack[s].b->sum) && (get_sumset_intersection_size(stack[s].a, stack[s].b) == 2))) {
+            stack[s].elif = true;
             if (stack[s].b->sum > best_solution->sum) {
                 solution_build(best_solution, input_data, stack[s].a, stack[s].b);
             }
@@ -65,13 +71,9 @@ int main()
 {
     InputData input_data;
     input_data_read(&input_data);
-
     Solution best_solution;
     solution_init(&best_solution);
-
     solve_iterative(&input_data, &best_solution);
     solution_print(&best_solution);
-
     return 0;
 }
-
